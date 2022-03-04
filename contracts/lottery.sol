5ce7393e67783cd7856c25e50d35b5b8b5e12f47
"ejyang" <"trustnloveu@gmail.com">
2022-03-04 14:07:18 +0900

    lottery contract

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -21,4 +21,23 @@ contract Lottery {
 
         return address(this).balance;
     }
+
+    function random() public view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager, "Not Allowed. Manager Only");
+        require(players.length >= 3, "Not Enough Players Has Joined Yet.");
+
+        uint randomNumber = random(); // Generate random number
+        address payable winner;
+
+        uint index = randomNumber % players.length;
+        winner = players[index]; // Pick winner
+
+        winner.transfer(getBalance()); // Transfer all ballance of this contract account to the winner
+
+        players = new address payable[](0); // Reset dynamic array with length 0
+    }
 }
