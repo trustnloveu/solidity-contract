9c25bd7fa0c90969fc723c8af3b89e031193b3a2
"ejyang" <"trustnloveu@gmail.com">
2022-03-15 12:56:02 +0900

    modify Will

--- contracts/will.sol
+++ contracts/will.sol
@@ -17,7 +17,12 @@ contract Will {
     constructor() payable {
         owner = msg.sender; // msg sender represents address being called
         fortune = msg.value; //msg value tells us how much ether is being sent  
+        deceased = false;
     }
+
+    // receive() external payable {}
+
+    // fallback() external payable {}
 
     /************************ Modifier ************************/
 
@@ -33,14 +38,25 @@ contract Will {
 
     /************************ Function ************************/    
 
-    // set inheritance for each address 
+    // 상속 리스트 설정(상속자 - 상속금)
+    function setInheritance(address payable wallet, uint amount) public onlyOwner {
+        familyWallets.push(wallet);
+        inheritance[wallet] = amount;
+    }
+
+    // 상속 멤버 수
+    function checkFamilyWalletLength() public view returns(uint) {
+        return familyWallets.length;
+    }
+
+    // 지급
     function payout() private mustBeDeceased {
         for(uint i=0; i<familyWallets.length; i++) {
             familyWallets[i].transfer(inheritance[familyWallets[i]]); // transfer funds from contract address to reciever address
         }
     }
 
-    // oracle switch
+    // Trigger
     function hasDeceased() public onlyOwner {
         deceased = true;
         payout();
